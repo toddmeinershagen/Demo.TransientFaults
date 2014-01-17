f67793bbeda7ad2fb31d6916c71fc26cb5aab69b
Todd Meinershagen <todd@meinershagen.net>
2014-01-17 08:46:21 -0600

    Created first test to test a simple call to a nancyfx rest-based service resource with httpclient.

---
+++ Demo.TransientFaults.WebApi.Tests/ReliableHttpClientTests.cs
@@ -0,0 +1,92 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Net.Http.Headers;
+
+namespace Demo.TransientFaults.WebApi.Tests
+{
+    [TestFixture]
+    public class ReliableHttpClientTests
+    {
+        private NancyHost _host;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _host = new NancyHost(new Uri("http://localhost:1234/api/"));
+            _host.Start();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _host.Stop();
+        }
+
+        [Test]
+        public async void given_one_call_to_service_it_should_fail_with_a_500_status_code()
+        {
+            string actualContent = null;
+
+            Action action = () =>
+                {
+                    var client = new HttpClient();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var url = string.Format("http://localhost:1234/api/Customers/{0}", 1);
+                    Task<HttpResponseMessage> response = client.GetAsync(url);
+
+                    var result = response.Result;
+
+                    Task<string> content = result.Content.ReadAsStringAsync();
+                    actualContent = content.Result;
+
+                    result.EnsureSuccessStatusCode();
+                };
+
+            action.ShouldThrow<HttpRequestException>().WithMessage("Response status code does not indicate success: 500 (Internal Server Error).");
+            actualContent.Should().Be("{\"Message\":\"Something went wrong\"}");
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        private int counter = 0;
+
+        public TestModule()
+        {
+            Get["/Customers/{id}"] = (parameters) =>
+                {
+                    if (++counter % 3 == 0)
+                    {
+                        return Negotiate
+                            .WithStatusCode(HttpStatusCode.OK)
+                            .WithModel(new Customer { Id = 1, FirstName = "Todd", LastName = "Meinershagen" });
+                    }
+
+                    return Negotiate
+                        .WithStatusCode(HttpStatusCode.InternalServerError)
+                        .WithModel(new Error { Message = "Something went wrong" });
+                };
+        }
+    }
+
+    public class Error
+    {
+        public string Message { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
