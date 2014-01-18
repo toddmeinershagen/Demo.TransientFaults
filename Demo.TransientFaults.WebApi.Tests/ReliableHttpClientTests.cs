3da0bc4093d3db2375dfacec9b8b2446de8d0d01
Todd Meinershagen <todd@meinershagen.net>
2014-01-18 09:30:40 -0600

    Added a transient error detection strategy and test to prove it with HttpClient.

--- Demo.TransientFaults.WebApi.Tests/ReliableHttpClientTests.cs
+++ Demo.TransientFaults.WebApi.Tests/ReliableHttpClientTests.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using System.Net.Http.Headers;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
 
 namespace Demo.TransientFaults.WebApi.Tests
 {
@@ -20,7 +21,11 @@ namespace Demo.TransientFaults.WebApi.Tests
         [SetUp]
         public void SetUp()
         {
-            _host = new NancyHost(new Uri("http://localhost:1234/api/"));
+            Singleton.Instance.Reset();
+
+            var config = new HostConfiguration { UrlReservations = new UrlReservations { CreateAutomatically = true} };
+            _host = new NancyHost(config, new Uri("http://localhost:1234/api/"));
+            
             _host.Start();
         }
 
@@ -31,7 +36,7 @@ namespace Demo.TransientFaults.WebApi.Tests
         }
 
         [Test]
-        public async void given_one_call_to_service_it_should_fail_with_a_500_status_code()
+        public void given_call_to_service_it_should_fail_with_a_500_status_code()
         {
             string actualContent = null;
 
@@ -54,17 +59,54 @@ namespace Demo.TransientFaults.WebApi.Tests
             action.ShouldThrow<HttpRequestException>().WithMessage("Response status code does not indicate success: 500 (Internal Server Error).");
             actualContent.Should().Be("{\"Message\":\"Something went wrong\"}");
         }
+
+        [Test]
+        public void given_call_to_service_with_incremental_retry_strategy_should_succeed_and_return_content()
+        {
+            var retryStrategy = new Incremental(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2));
+            var retryPolicy = new RetryPolicy<HttpClientTransientErrorDetectionStrategy>(retryStrategy);
+
+            string actualContent = null;
+
+            Action innerAction = () =>
+                {
+                    var client = new HttpClient();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var url = string.Format("http://localhost:1234/api/Customers/{0}", 1);
+                    Task<HttpResponseMessage> response = client.GetAsync(url);
+
+                    var result = response.Result;
+
+                    Task<string> content = result.Content.ReadAsStringAsync();
+                    actualContent = content.Result;
+
+                    result.EnsureSuccessStatusCode();
+                };
+
+            Action action = () => retryPolicy.ExecuteAction(innerAction);
+
+            action.ShouldNotThrow<HttpRequestException>();
+            actualContent.Should().Be("{\"Id\":1,\"FirstName\":\"Todd\",\"LastName\":\"Meinershagen\"}");
+        }
+    }
+
+    public class HttpClientTransientErrorDetectionStrategy: ITransientErrorDetectionStrategy
+    {
+        public bool IsTransient(Exception ex)
+        {
+            var httpException = ex as HttpRequestException;
+            return httpException != null;
+        }
     }
 
     public class TestModule : NancyModule
     {
-        private int counter = 0;
-
         public TestModule()
         {
             Get["/Customers/{id}"] = (parameters) =>
                 {
-                    if (++counter % 3 == 0)
+                    if (++Singleton.Instance.Counter % 3 == 0)
                     {
                         return Negotiate
                             .WithStatusCode(HttpStatusCode.OK)
@@ -75,6 +117,32 @@ namespace Demo.TransientFaults.WebApi.Tests
                         .WithStatusCode(HttpStatusCode.InternalServerError)
                         .WithModel(new Error { Message = "Something went wrong" });
                 };
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public int Counter { get; set; }
+
+        public void Reset()
+        {
+            Counter = 0;
         }
     }
 
